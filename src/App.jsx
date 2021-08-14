b8b8756b6fb96502c4e108ec9622d083d42257a5
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-08-14 19:26:58 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import './App.css'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Filtering system</h1>
+    </div>
+  )
+}
+
+export default App
